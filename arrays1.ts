aca0f095ab6edba4a4e6e56f951f6b9cdb57429b
jonathanbraverman <109186475+jonathanbraverman@users.noreply.github.com>
2022-10-10 11:33:28 -0400

    Added comments and example regarding declaring an empty array and assigning elements.

--- arrays1.ts
+++ arrays1.ts
@@ -5,11 +5,11 @@ for(var i = 0; i < 5;i++)
     console.log(array1[i])
 }
 
-let array2:number[] = new Array(5)
+let array2:number[] = new Array(5)  // Declaring an empty array with 5 elements
 
 for(var i = 0; i < 5;i++)
 {
-    //array2[i] = i
+    array2[i] = i                   // Assign values 0 to 4 to elements 1 to 5
     console.log(array2[i])
 }
 
